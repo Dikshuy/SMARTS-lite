690249cd957bf3ec67becf76ff45df2493328a49
Dikshuy <dikshant1212hsr@gmail.com>
2021-07-03 03:46:40 -0400

    shell file updated

--- job.sh
+++ job.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-#SBATCH --account=def-mtaylor3
 #SBATCH --time=00:20:00
-#SBATCH --job-name=test
-#SBATCH --output=%x-%j.out
-cd ~/projects/def-mtaylor3/dikshant/
-singularity shell --bind SMARTS-lite/:/SMARTS-lite --env DISPLAY=$DISPLAY smarts-0416_singularity.sif
-cd /SMARTS-lite/ultra/
-export PYTHONPATH=/SMARTS-lite/ultra:/SMARTS-lite/:$PYTHONPATH
-python ultra/hammer_train.py --task 0-3agents --level easy --episodes 10 --eval-episodes 2 --eval-rate 5 --policy ppo,ppo,ppo --headless
-
+#SBATCH --output=slurm-%j.out
+#SBATCH --account=def-mtaylor3
+module load singularity
+singularity exec -B ../SMARTS-lite:/SMARTS-lite --env DISPLAY=$DISPLAY,PYTHONPATH=/SMARTS-lite/ultra:/src --home /SMARTS-lite/ultra ../smarts-0416_singularity.sif python ultra/hammer_train.py --task 0-3agents --level easy --episodes 10 --eval-episodes 2 --eval-rate 5 --policy ppo,ppo,ppo --headless
