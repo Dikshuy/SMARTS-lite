c41a6127266b30bce5fb6930209d768b709e25bf
Dikshuy <dikshant1212hsr@gmail.com>
2021-07-06 02:11:57 +0530

    added gpu configs to jobs

--- job_without_traffic.sh
+++ job_without_traffic.sh
@@ -3,7 +3,9 @@
 #SBATCH --output=slurm-%j.out
 #SBATCH --account=def-mtaylor3
 #SBATCH --mem=128G
+#SBATCH --gres=gpu:v100:4
 #SBATCH --cpus-per-task=16
 
 module load singularity
+module load arch/avx512 StdEnv/2018.3
 singularity exec -B ../SMARTS-lite:/SMARTS-lite --env DISPLAY=$DISPLAY,PYTHONPATH=/SMARTS-lite/ultra:/SMARTS-lite:$PYTHONPATH --home /SMARTS-lite/ultra ../smarts-0416_singularity.sif python ultra/hammer_train.py --task 0-3agents --level no-traffic --policy ppo,ppo,ppo --headless
