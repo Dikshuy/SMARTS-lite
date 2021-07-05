6b5f66567a971672e432004a7bbc5d15906c5e18
Dikshuy <dikshant1212hsr@gmail.com>
2021-07-05 17:13:31 -0400

    jobs updated from CC

--- job_without_traffic.sh
+++ job_without_traffic.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH --time=24:00:00
+#SBATCH --time=1-12:00:00
 #SBATCH --output=slurm-%j.out
 #SBATCH --account=def-mtaylor3
-#SBATCH --mem=128G
-#SBATCH --gres=gpu:v100:4
-#SBATCH --cpus-per-task=16
+#SBATCH --mem=32000M
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=2
 
 module load singularity
-module load arch/avx512 StdEnv/2018.3
 singularity exec -B ../SMARTS-lite:/SMARTS-lite --env DISPLAY=$DISPLAY,PYTHONPATH=/SMARTS-lite/ultra:/SMARTS-lite:$PYTHONPATH --home /SMARTS-lite/ultra ../smarts-0416_singularity.sif python ultra/hammer_train.py --task 0-3agents --level no-traffic --policy ppo,ppo,ppo --headless
